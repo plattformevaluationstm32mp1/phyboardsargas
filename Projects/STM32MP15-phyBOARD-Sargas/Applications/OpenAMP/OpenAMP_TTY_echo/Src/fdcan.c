93ef8028642df94e11b05e137764fd6e43bcc6ee
tengler <thomas.engler@solve.ch>
2023-03-28 12:46:00 +0200

    CAN FD parameter updated

--- Projects/STM32MP15-phyBOARD-Sargas/Applications/OpenAMP/OpenAMP_TTY_echo/Src/fdcan.c
+++ Projects/STM32MP15-phyBOARD-Sargas/Applications/OpenAMP/OpenAMP_TTY_echo/Src/fdcan.c
@@ -38,7 +38,7 @@ void MX_FDCAN2_Init(void)
 
   /* USER CODE END FDCAN2_Init 1 */
   hfdcan2.Instance = FDCAN2;
-  hfdcan2.Init.FrameFormat = FDCAN_FRAME_FD_NO_BRS;
+  hfdcan2.Init.FrameFormat = FDCAN_FRAME_FD_BRS;
   hfdcan2.Init.Mode = FDCAN_MODE_NORMAL;
   hfdcan2.Init.AutoRetransmission = ENABLE;
   hfdcan2.Init.TransmitPause = DISABLE;
@@ -47,10 +47,10 @@ void MX_FDCAN2_Init(void)
   hfdcan2.Init.NominalSyncJumpWidth = 1;
   hfdcan2.Init.NominalTimeSeg1 = 5;
   hfdcan2.Init.NominalTimeSeg2 = 2;
-  hfdcan2.Init.DataPrescaler = 3;
+  hfdcan2.Init.DataPrescaler = 1;
   hfdcan2.Init.DataSyncJumpWidth = 1;
-  hfdcan2.Init.DataTimeSeg1 = 5;
-  hfdcan2.Init.DataTimeSeg2 = 2;
+  hfdcan2.Init.DataTimeSeg1 = 4;
+  hfdcan2.Init.DataTimeSeg2 = 1;
   hfdcan2.Init.MessageRAMOffset = 0;
   hfdcan2.Init.StdFiltersNbr = 0;
   hfdcan2.Init.ExtFiltersNbr = 0;
